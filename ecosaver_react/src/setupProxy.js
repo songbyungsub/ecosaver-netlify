70e958d9ee2c195cd6fcac095d1a9296dfc45d25
byungsub <nq8947@naver.com>
2024-03-05 17:12:08 +0900

    axios

---
+++ ecosaver_react/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      // 백엔드 주소
+      target: "http://127.0.0.1:8000",
+      changeOrigin: true,
+    })
+  );
+};
